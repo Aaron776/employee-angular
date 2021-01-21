9b5a68b3e531b9ae353705f5524d0082a4eab0da
Aaron Ortiz <aronortiz90@yahoo.com>
2021-01-21 10:19:28 -0500

    pantalla login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent} from './login/login.component';
+import { AuthService} from './services/auth.service';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+    declarations: [
+    AppComponent,
+    LoginComponent
+    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+
   ],
-  providers: [],
+  providers: [
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
