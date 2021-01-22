d93b97ac4a4fcc641a0f1e00e97f6f1b4dd4ae69
Aaron Ortiz <aronortiz90@yahoo.com>
2021-01-22 15:14:30 -0500

    agregar api para editar actividad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,20 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent} from './login/login.component';
-import { AuthService} from './services/auth.service';
+import { LoginComponent } from './login/login.component';
+import { AuthService } from './services/auth.service';
 
 import { ListActivityComponent } from './list-activity/list-activity.component';
-import {ActivityService} from './services/activity.service';
-import {EmployeeService}  from './services/employee.service';
-
+import { ActivityService } from './services/activity.service';
+import { EmployeeService } from './services/employee.service';
+import { DetailActivityComponent } from './detail-activity/detail-activity.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListActivityComponent,
-    LoginComponent
+    LoginComponent,
+    DetailActivityComponent
   ],
 
   imports: [
