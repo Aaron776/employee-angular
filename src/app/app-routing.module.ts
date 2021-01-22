99cfc32562608b081b60defb1cc38dad3f76e390
Aaron Ortiz <aronortiz90@yahoo.com>
2021-01-21 23:03:26 -0500

    Merge remote-tracking branch 'origin/master'
    arreglos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-<<<<<<< HEAD
 import {LoginComponent} from './login/login.component';
 
 const routes: Routes = [
 { path: '', component: LoginComponent },
 
-=======
-import {ListActivityComponent} from './list-activity/list-activity.component';
-
-const routes: Routes = [
-  { path: 'list/:id/:name/:lastName', component:ListActivityComponent},
-  
->>>>>>> master
 ];
 
 @NgModule({
